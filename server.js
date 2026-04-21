3a8b5ad26b65ef26e61a5f28a479942cdbddc193
B JESWANTH REDDY <jeswanth.jrjohn@gmail.com>
2026-04-21 20:29:03 +0530

    feat: implement JWT authentication and role-based authorization (admin access control)

--- server.js
+++ server.js
@@ -3,27 +3,48 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+// middleware
+const { protect, isAdmin } = require('./middleware/authMiddleware');
+
+// routes
+const authRoutes = require('./routes/authRoutes');
+
 dotenv.config(); 
 
 const app = express(); 
 
-//middleware
+// middleware
 app.use(express.json()); 
 app.use(cors()); 
 
-//test routs
+app.use('/api/auth', authRoutes);
+
+// test routes
 app.get('/', (req, res) => {
-    res.send('API running'); 
+    res.send('THIS IS MY SERVER');
+});
+
+app.get('/api/protected', protect, (req, res) => {
+  res.json({
+    message: 'You are authorized',
+    user: req.user,
+  });
+});
+
+app.get('/api/admin', protect, isAdmin, (req, res) => {
+  res.json({
+    message: 'Welcome Admin',
+  });
 });
 
 const PORT = process.env.PORT || 5000; 
 
-//DB connect + server start
+// DB connect + server start
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('MongoDB connected'); 
     app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);         
+        console.log(`Server running on port ${PORT}`);         
     }); 
 })
-.catch(err => console.log(err)); 
+.catch(err => console.log(err));
