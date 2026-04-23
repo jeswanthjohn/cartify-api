73c9defb14883b723cea809002ab62ed822e8f39
B JESWANTH REDDY <jeswanth.jrjohn@gmail.com>
2026-04-23 19:36:28 +0530

    feat: implement product creation and retrieval with MongoDB

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,42 @@
-const getProducts = (req, res) => {
-    res.json([
-        {id: 1, name: 'Sample Product', price: 100}
-    ]);
-}; 
+const Product = require('../models/productModel');
 
-const createProduct = (req, res) => {
-    res.json({
-        message: 'Product created (dummy)'
+// GET all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// CREATE product
+const createProduct = async (req, res) => {
+  try {
+    console.log('createProduct called');
+
+    const { name, price, description, category, countInStock } = req.body;
+
+    const product = new Product({
+      name,
+      price,
+      description,
+      category,
+      countInStock,
     });
+
+    const savedProduct = await product.save();
+
+    console.log('product saved');
+
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    getProducts, 
-    createProduct,
-}
+  getProducts,
+  createProduct,
+};
